986466c514d65f501f406a672bca86b29d890ee0
eunbinoh <nohonbi01@gmail.com>
2021-09-28 18:30:42 +0900

    todo list up

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" placeholder="What I have to Do?" v-on:keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+        </span>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                newTodoItem:''
+            }
+        },
+        methods:{
+            addTodo(){
+                if(this.newTodoItem!==""){
+                    var val = this.newTodoItem && this.newTodoItem.trim();
+                    localStorage.setItem(val, "input: "+val);
+                    this.clearInput();
+                }
+            },
+            clearInput(){
+                this.newTodoItem='';
+            }
+        }
+    }
+</script>
+
+<style scoped>
+ input:focus{
+    outline: none;
+ }
+ .inputBox{
+    background-color: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+ }
+ .inputBox input{
+    border-style: none;
+    font-size: 1.5rem;
+    vertical-align: middle;
+ }
+ .addContainer {
+    float: right;
+    background: linear-gradient(to right, #a26fc4, #8143bb);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+ }
+ .addBtn{
+    color: whitesmoke;
+    vertical-align: middle;
+ }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,44 @@
+<template>
+    <section>
+        <ul>
+            <li v-for="todoItem in todoItems">{{ todoItem }}</li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            todoItems:[]
+        }
+    },
+    created(){
+        if(localStorage.length>0){
+            for(var i=0; i<localStorage.length; i++){
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    },
+}
+</script>
+
+<style scoped>
+    ul {
+        list-style-type: none;
+        padding-left: 0;
+        margin-top: 0;
+        text-align: center;
+        font-size: 1.3rem;
+    }
+    li {
+        display: flex;
+        min-height: 40px;
+        height: 40px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding: 0 0 0 10%;
+        background: white;
+        border-radius: 5px;
+    }
+</style>
